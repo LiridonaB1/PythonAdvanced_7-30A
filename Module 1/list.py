a98fd7c331043454c3b2ec5b84c7b1368765d0b6
LiridonaB1 <liridona.baliu@digitalschool.tech>
2026-03-23 20:41:55 +0100

    module 2

---
+++ Module 1/list.py
@@ -0,0 +1,22 @@
+shopping_list = ["apples","bananas",3,4.5]
+
+print(shopping_list[1])
+
+shopping_list.append("oranges")
+print(shopping_list)
+
+shopping_list.insert(2,"lemons")
+print(shopping_list)
+
+shopping_list.remove("oranges")
+print(shopping_list)
+
+del shopping_list[2]
+print(shopping_list)
+
+shopping_list[0] = "cherry"
+print(shopping_list)
+
+
+
+
