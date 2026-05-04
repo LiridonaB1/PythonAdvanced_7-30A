0ca4ed42bad72b816f3977e71c5528a76dfb34b7
LiridonaB1 <liridona.baliu@digitalschool.tech>
2026-05-04 20:33:48 +0200

    module 11

---
+++ Module 11/datatime_examples.py
@@ -0,0 +1,14 @@
+import datetime
+
+current_datetime = datetime.datetime.now()
+
+print("Year:",current_datetime.year)
+print("Month:",current_datetime.month)
+print("Day:",current_datetime.day)
+
+current_date = datetime.datetime.now().date()
+print(current_date)
+
+current_time = datetime.datetime.now().time()
+print(current_time)
+
