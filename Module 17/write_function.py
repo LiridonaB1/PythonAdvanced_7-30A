fa63c5ec61a1788ea711c24a06618a5fb04dd6cb
LiridonaB1 <liridona.baliu@digitalschool.tech>
2026-06-03 20:34:12 +0200

    Module 18

---
+++ Module 17/write_function.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.DataFrame({
+    'Name': ['Alice','Bob','Charlie'],
+    'Age': [24,22,27],
+    'City':['Los Angeles','New York','Prishtine']
+
+})
+
+st.write(df)
